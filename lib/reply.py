16185a67299a3059fa4766a6742bbb1f3a29e19a
Elijah <57646863+deftclaw@users.noreply.github.com>
2024-10-09 12:00:31 -0600

    Update reply.py
    Allow passing errors directly to reply, for error-logging

--- lib/reply.py
+++ lib/reply.py
@@ -18,17 +18,36 @@ elif name == 'posix':
 else:
     raise ValueError(f'{name} not yet supported.')
 
+EC = {
+    'is_error': lambda msg: True if str(msg.__class__.__mro__[-2]).split("'")[1] == 'BaseException' else False,
+    'get': lambda err: str(err.__class__).split("'")[1],
+    'FileNotFoundError': lambda err: f"{err.args[1]}: {err.filename}",
+    'KeyError': lambda err: f"\'{err.args[0]}\' is not defined",
+    'NameError': lambda err: err.args[0].replace('name ', ''),
+    'ValueError': lambda err: err.args[0]
+}
+
 # Write message to screen and log, with message-level-colors on screen
 def reply(message, type='info', leaf='reply.log'):
-    message = str(message).rstrip()
-
+    out_message = str(message).rstrip()
     with open(leaf, "a") as log_file:
         if type in ['error', 'err', 'e']:
-            print('%sERROR: %s %s' % (COL['err'], message, COL['none']))
+            if EC['is_error'](message):
+                print('%sERROR: %s %s' % (COL['err'], get_error(message), COL['none']))
+            else:
+                print('%sERROR: %s %s' % (COL['err'], out_message, COL['none']))
             log_file.write(f'ERROR: {message}\n')
         elif type in ['warning', 'warn', 'w']:
-            print('%sWARNING: %s%s' % (COL['warn'], message, COL['none']))
+            print('%sWARNING: %s%s' % (COL['warn'], out_message, COL['none']))
             log_file.write(f'WARNING: {message}\n')
         else:
-            print('%s%s%s' % (COL['info'], message, COL['none']))
-            log_file.write(f'INFO: {message}\n')
+            print('%s%s%s' % (COL['info'], out_message, COL['none']))
+            log_file.write(f'INFO: {out_message}\n')
+
+def get_error(err):  # Get a descriptive error-message
+    klass = EC['get'](err)
+
+    try:
+        return klass + ': ' + EC[klass](err)
+    except KeyError as k:
+        reply(EC['KeyError'](k), 'e', 'reply-errors.log')
